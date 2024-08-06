e5c812cffa22174f05f7493257766893b926fc08
Luis2Aliive <rezzutto12@gmail.com>
2024-08-06 14:10:58 -0400

    I have almost met all the requirments for Javascript

---
+++ html_1/javascript.js
@@ -0,0 +1,36 @@
+alert("Welcome To The Official Release of VerseVibe!");
+
+// Integer
+let numberofParticipants = 50;
+
+// String
+let workshopTitle = "Coding Best Practices"
+
+// Float
+let averageAgeOfParticipants = 25.4;
+
+// Boolean
+let isWorkshopOnline = true;
+
+
+
+console.log(numberofParticipants);
+
+
+let numberTshirts = 45;
+let numberPants = 35;
+let totalClothes = numberTshirts + numberPants;
+console.log(totalClothes);
+
+if (numberTshirts >= 40) {
+    console.log("We don't need anymore Tshirts");
+} else {
+    console.log("We do need more Tshirts");
+}
+
+let numberSweatshirts = 30;
+let numberShorts = 50;
+
+if (numberSweatshirts <= 40 && numberShorts >= 35) {
+    console.log("We have enough clothes");
+}
